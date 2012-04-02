ef3841893a8c5d47d2ae1a1b86dd9d6cd3a74f69
Oldrich Kruza <sixtease+github@gmail.com>
2012-04-02 20:06:18 +0200

    Allowed filtering by all columns

--- scribble/resources/__init__.py
+++ scribble/resources/__init__.py
@@ -10,6 +10,17 @@ class CategoryResource(ModelResource):
     class Meta:
         authorization = Authorization()
         queryset = Category.objects.all()
+        filtering = {
+            'app_data'         : ('exact',),
+            'content'          : ('exact',),
+            'description'      : ('exact',),
+            'id'               : ('exact',),
+            'resource_uri'     : ('exact',),
+            'slug'             : ('exact',),
+            'template'         : ('exact',),
+            'title'            : ('exact',),
+            'tree_path'        : ('exact',),
+        }
 
 
 class PhotoResource(ModelResource):
@@ -17,19 +28,49 @@ class PhotoResource(ModelResource):
         authorization = Authorization()
         queryset = Photo.objects.all()
         resource_name = 'photo'
-        excludes = ['slug']  # trying to redefine photo resource from testapp
+        filtering = {
+            'app_data'         : ('exact',),
+            'created'          : ('exact',),
+            'description'      : ('exact',),
+            'height'           : ('exact',),
+            'id'               : ('exact',),
+            'image'            : ('exact',),
+            'important_bottom' : ('exact',),
+            'important_left'   : ('exact',),
+            'important_right'  : ('exact',),
+            'important_top'    : ('exact',),
+            'resource_uri'     : ('exact',),
+            'title'            : ('exact',),
+            'width'            : ('exact',),
+        }
 
 
 class ListingResource(ModelResource):
     class Meta:
         authorization = Authorization()
         queryset = Listing.objects.all()
+        filtering = {
+            'commercial'       : ('exact',),
+            'id'               : ('exact',),
+            'publish_from'     : ('exact',),
+            'publish_to'       : ('exact',),
+            'resource_uri'     : ('exact',),
+        }
 
 
 class AuthorResource(ModelResource):
     class Meta:
         authorization = Authorization()
         queryset = Author.objects.all()
+        filtering = {
+            'description'      : ('exact',),
+            'email'            : ('exact',),
+            'id'               : ('exact',),
+            'name'             : ('exact',),
+            'resource_uri'     : ('exact',),
+            'slug'             : ('exact',),
+            'text'             : ('exact',),
+        }
 
 
 class PublishableResource(ModelResource):
@@ -42,6 +83,23 @@ class PublishableResource(ModelResource):
         authorization = Authorization()
         queryset = Publishable.objects.all()
         resource_name = 'publishable'
+        filtering = {
+            'announced'        : ('exact',),
+            'app_data'         : ('exact',),
+            'authors'          : ('exact',),
+            'category'         : ('exact',),
+            'description'      : ('exact',),
+            'id'               : ('exact',),
+            'listings'         : ('exact',),
+            'photo'            : ('exact',),
+            'publish_from'     : ('exact',),
+            'publish_to'       : ('exact',),
+            'published'        : ('exact',),
+            'resource_uri'     : ('exact',),
+            'slug'             : ('exact',),
+            'static'           : ('exact',),
+            'title'            : ('exact',),
+        }
 
     def dehydrate(self, bundle):
         bundle.data['url'] = bundle.obj.get_domain_url()
@@ -54,3 +112,24 @@ class ArticleResource(PublishableResource):
         authorization = Authorization()
         queryset = Article.objects.all()
         resource_name = 'article'
+        filtering = {
+            'announced'        : ('exact',),
+            'app_data'         : ('exact',),
+            'authors'          : ('exact',),
+            'category'         : ('exact',),
+            'content'          : ('exact',),
+            'created'          : ('exact',),
+            'description'      : ('exact',),
+            'id'               : ('exact',),
+            'listings'         : ('exact',),
+            'photo'            : ('exact',),
+            'publish_from'     : ('exact',),
+            'publish_to'       : ('exact',),
+            'published'        : ('exact',),
+            'resource_uri'     : ('exact',),
+            'slug'             : ('exact',),
+            'static'           : ('exact',),
+            'title'            : ('exact',),
+            'updated'          : ('exact',),
+            'upper_title'      : ('exact',),
+        }
